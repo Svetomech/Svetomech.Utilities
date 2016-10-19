5d436b794cfa519cb86c7c05a9bf64d8cdbdb082
Andrey Belyaev <svetomech@gmail.com>
2016-10-19 23:07:04 +0300

    Changed tabs to spaces

--- Svetomech.Utilities/Window.cs
+++ Svetomech.Utilities/Window.cs
@@ -4,50 +4,50 @@ using static Svetomech.Utilities.SimplePlatform;
 
 namespace Svetomech.Utilities
 {
-	public class Window
-	{
-		public IntPtr Handle { get; set; } = IntPtr.Zero;
+    public class Window
+    {
+        public IntPtr Handle { get; set; } = IntPtr.Zero;
 
-		public Window() { }
+        public Window() { }
 
-		public Window(IntPtr handle)
-		{
-			Handle = handle;
-		}
+        public Window(IntPtr handle)
+        {
+            Handle = handle;
+        }
 
-		public bool Hide() => runningWindows ? WindowsWindow.Hide(this) : LinuxWindow.Hide(this);
+        public bool Hide() => runningWindows ? WindowsWindow.Hide(this) : LinuxWindow.Hide(this);
 
-		public bool Show() => runningWindows ? WindowsWindow.Show(this) : LinuxWindow.Show(this);
+        public bool Show() => runningWindows ? WindowsWindow.Show(this) : LinuxWindow.Show(this);
 
-		private readonly bool runningWindows = (RunningPlatform() == Platform.Windows);
+        private readonly bool runningWindows = (RunningPlatform() == Platform.Windows);
 
 
-		private class WindowsWindow
-		{
-			internal static bool Hide(Window window)
-			{
-				int SW_HIDE = 0;
+        private class WindowsWindow
+        {
+            internal static bool Hide(Window window)
+            {
+                int SW_HIDE = 0;
 
-				return ShowWindow(window.Handle, SW_HIDE);
-			}
+                return ShowWindow(window.Handle, SW_HIDE);
+            }
 
-			internal static bool Show(Window window)
-			{
-				int SW_SHOW = 5;
+            internal static bool Show(Window window)
+            {
+                int SW_SHOW = 5;
 
-				return ShowWindow(window.Handle, SW_SHOW);
-			}
+                return ShowWindow(window.Handle, SW_SHOW);
+            }
 
-			[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-			private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-		}
+            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        }
 
-		// TODO: Find the actual Linux APIs equivalent to Windows ones
-		private class LinuxWindow
-		{
-			internal static bool Hide(Window window) => true;
+        // TODO: Find the actual Linux APIs equivalent to Windows ones
+        private class LinuxWindow
+        {
+            internal static bool Hide(Window window) => true;
 
-			internal static bool Show(Window window) => true;
-		}
-	}
+            internal static bool Show(Window window) => true;
+        }
+    }
 }
