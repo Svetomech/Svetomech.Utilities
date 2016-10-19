5dca7f0952d52c418f654a9f272a612ed3e9062b
Andrey Belyaev <svetomech@gmail.com>
2016-10-19 23:06:27 +0300

    Removed ShowWindow() from NativeMethods, added GetVisibleWindows() extension method

--- Svetomech.Utilities/NativeMethods.cs
+++ Svetomech.Utilities/NativeMethods.cs
@@ -11,13 +11,6 @@ namespace Svetomech.Utilities
             return runningWindows ? WindowsNative.GetConsoleWindow() : LinuxNative.GetConsoleWindow();
         }
 
-        public static bool ShowWindow(IntPtr hWnd, int nCmdShow)
-        {
-            return runningWindows ? WindowsNative.ShowWindow(hWnd, nCmdShow) : LinuxNative.ShowWindow(hWnd, nCmdShow);
-        }
-        public const int SW_HIDE = 0;
-        public const int SW_SHOW = 5;
-
         private static readonly bool runningWindows = (RunningPlatform() == Platform.Windows);
 
 
@@ -25,17 +18,12 @@ namespace Svetomech.Utilities
         {
             [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
             internal static extern IntPtr GetConsoleWindow();
-
-            [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-            internal static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
         }
 
         // TODO: Find the actual Linux APIs equivalent to Windows ones
         private static class LinuxNative
         {
             internal static IntPtr GetConsoleWindow() => IntPtr.Zero;
-
-            internal static bool ShowWindow(IntPtr hWnd, int nCmdShow) => true;
         }
     }
 }
