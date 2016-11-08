70700c7d4214dc339d335f55182ffaddf58661e4
Andrey Belyaev <svetomech@gmail.com>
2016-11-09 01:41:04 +0300

    Finished migrating to NUnit

---
+++ Svetomech.Utilities.Tests/SimpleAppTest.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using static Svetomech.Utilities.SimpleApp;
+
+namespace Svetomech.Utilities.Tests
+{
+    [TestFixture]
+    public class SimpleAppTest
+    {
+        [Test]
+        public void IsElevated_False()
+        {
+            Assert.IsFalse(IsElevated());
+        }
+
+        [Test, Order(1)]
+        public void VerifyAutorun_SomeApp_False()
+        {
+            string appName = "SomeApp";
+            string appPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                appName, $"{appName}.exe");
+
+            bool isAppAutorun = VerifyAutorun(appName, appPath);
+
+            Assert.IsFalse(isAppAutorun);
+        }
+
+        [Test, Order(2)]
+        public void SwitchAutorun_OtherApp_Add()
+        {
+            string appName = "OtherApp";
+            string appPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                appName, $"{appName}.exe");
+
+            try
+            {
+                SwitchAutorun(appName, appPath);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test, Order(3)]
+        public void VerifyAutorun_OtherApp_True()
+        {
+            string appName = "OtherApp";
+            string appPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                appName, $"{appName}.exe");
+
+            bool isAppAutorun = VerifyAutorun(appName, appPath);
+
+            Assert.IsTrue(isAppAutorun);
+        }
+
+        [Test, Order(4)]
+        public void SwitchAutorun_OtherApp_Remove()
+        {
+            string appName = "OtherApp";
+
+            try
+            {
+                SwitchAutorun(appName);
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
