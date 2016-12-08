986d96d4e9f34406781e602d30f463b7b5be06c3
Andrey Belyaev <svetomech@gmail.com>
2016-12-08 20:38:03 +0300

    Added two methods to SimpleString

--- Svetomech.Utilities/SimpleString.cs
+++ Svetomech.Utilities/SimpleString.cs
@@ -1,11 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Text.RegularExpressions;
 using static System.String;
 
 namespace Svetomech.Utilities
 {
     public static class SimpleString
     {
+        public static string UrlToFileNameDropbox(string url)
+        {
+            return Uri.UnescapeDataString(url.Substring(url.LastIndexOf('/') + 1));
+        }
+
+        public static string DecodeNonAsciiCharacters(string value)
+        {
+            return Regex.Replace(value, @"\\u(?<Value>[a-zA-Z0-9]{4})", m =>
+                    ((char)int.Parse(m.Groups["Value"].Value, NumberStyles.HexNumber)).ToString());
+        }
+
         public static string Pacmanise(this string str, int startIndex, char escapeChar)
         {
             if (IsNullOrWhiteSpace(str))
